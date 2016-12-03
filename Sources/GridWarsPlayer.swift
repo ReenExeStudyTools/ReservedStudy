25de62016b1a49b86cc221d628253443dc29df83
ReenExe <ipourri@gmail.com>
2016-12-03 23:22:58 +0200

    up common code

--- Sources/GridWarsPlayer.swift
+++ Sources/GridWarsPlayer.swift
@@ -1,9 +1,21 @@
 class GridWarsPlayer {
     private let current = 1 << 31;
+
     private let indexes = [
             0,      1,          2,
             3,      /* self */  5,
             6,      7,          8,
+    ];
+
+    let diagonalPriorityStep: [Int: Int] = [
+            6: 2,
+            2: 6,
+            8: 0,
+            0: 8,
+            7: 1,
+            1: 7,
+            5: 3,
+            3: 5,
     ];
 
     func play(matrix: [Int]) -> Array<Int> {
@@ -25,17 +37,6 @@ class GridWarsPlayer {
             if selfMatrix.isEmpty {
                 return [2, current]
             }
-
-            let diagonalPriorityStep: [Int: Int] = [
-                6: 2,
-                2: 6,
-                8: 0,
-                0: 8,
-                7: 1,
-                1: 7,
-                5: 3,
-                3: 5,
-            ];
 
             for (from, to) in diagonalPriorityStep {
                 if selfMatrix.contains(from) && freeMatrix.contains(to) {
