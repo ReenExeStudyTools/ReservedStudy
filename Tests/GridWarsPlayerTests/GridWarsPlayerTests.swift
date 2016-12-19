8a9cdd48b88ccf53bfae5d225c544d2c20773f4e
ReenExe <ipourri@gmail.com>
2016-12-19 20:46:21 +0200

    autoformating

--- Tests/GridWarsPlayerTests/GridWarsPlayerTests.swift
+++ Tests/GridWarsPlayerTests/GridWarsPlayerTests.swift
@@ -2,195 +2,195 @@ import XCTest
 @testable import GridWarsPlayer
 
 class GridWarsPlayerTests: XCTestCase {
-  func testSimple() {
-    let scout = 1 << 31 | 1 << 27 | 1 << 23
-    let player = GridWarsPlayer()
-    XCTAssertEqual(
-      player.play(
-        [
-          0, 0, 0,
-          0, 0, 0,
-          0, 0, 0,
-        ]
-      ),
-      [2, scout]
-    )
+    func testSimple() {
+        let scout = 1 << 31 | 1 << 27 | 1 << 23
+        let player = GridWarsPlayer()
+        XCTAssertEqual(
+            player.play(
+                [
+                    0, 0, 0,
+                    0, 0, 0,
+                    0, 0, 0,
+                ]
+            ),
+            [2, scout]
+        )
 
-    XCTAssertEqual(
-      player.play(
-        [
-          0,     0, 0,
-          0,     0, 0,
-          scout, 0, 0,
-        ]
-      ),
-      [2, scout]
-    )
+        XCTAssertEqual(
+            player.play(
+                [
+                    0, 0, 0,
+                    0, 0, 0,
+                    scout, 0, 0,
+                ]
+            ),
+            [2, scout]
+        )
 
-    XCTAssertEqual(
-      player.play(
-        [
-          0, 0, scout,
-          0, 0, 0,
-          0, 0, 0,
-        ]
-      ),
-      [6, scout]
-    )
+        XCTAssertEqual(
+            player.play(
+                [
+                    0, 0, scout,
+                    0, 0, 0,
+                    0, 0, 0,
+                ]
+            ),
+            [6, scout]
+        )
 
-    XCTAssertEqual(
-      player.play(
-        [
-          scout, 0, 0,
-          0,       0, 0,
-          0,       0, 0,
-        ]
-      ),
-      [8, scout]
-    )
+        XCTAssertEqual(
+            player.play(
+                [
+                    scout, 0, 0,
+                    0, 0, 0,
+                    0, 0, 0,
+                ]
+            ),
+            [8, scout]
+        )
 
-    XCTAssertEqual(
-      player.play(
-        [
-          scout, 0, 0,
-          0,     0, 0,
-          0,     0,  scout,
-        ]
-      ),
-      [1, scout]
-    )
+        XCTAssertEqual(
+            player.play(
+                [
+                    scout, 0, 0,
+                    0, 0, 0,
+                    0, 0, scout,
+                ]
+            ),
+            [1, scout]
+        )
 
-    let support = scout | 1 << 28
-    XCTAssertEqual(
-      player.play(
-        [
-          scout, scout, scout,
-          scout, scout, scout,
-          scout, scout, scout,
-        ]
-      ),
-      [0, support]
-    )
+        let support = scout | 1 << 28
+        XCTAssertEqual(
+            player.play(
+                [
+                    scout, scout, scout,
+                    scout, scout, scout,
+                    scout, scout, scout,
+                ]
+            ),
+            [0, support]
+        )
 
-    XCTAssertEqual(
-      player.play(
-        [
-          support, scout, scout,
-          scout,   scout, scout,
-          scout,   scout, scout,
-        ]
-      ),
-      [1, support]
-    )
+        XCTAssertEqual(
+            player.play(
+                [
+                    support, scout, scout,
+                    scout, scout, scout,
+                    scout, scout, scout,
+                ]
+            ),
+            [1, support]
+        )
 
-    XCTAssertEqual(
-      player.play(
-        [
-          support, support, support,
-          support, support, support,
-          support, support, scout,
-        ]
-      ),
-      [8, support]
-    )
+        XCTAssertEqual(
+            player.play(
+                [
+                    support, support, support,
+                    support, support, support,
+                    support, support, scout,
+                ]
+            ),
+            [8, support]
+        )
 
-    XCTAssertEqual(
-      player.play(
-        [
-          support, support, scout,
-          support, support, support,
-          support, support, support,
-        ]
-      ),
-      [2, support]
-    )
+        XCTAssertEqual(
+            player.play(
+                [
+                    support, support, scout,
+                    support, support, support,
+                    support, support, support,
+                ]
+            ),
+            [2, support]
+        )
 
-    let supportDouble = support + (1 << 28)
-    XCTAssertEqual(
-      player.play(
-        [
-          support, support, support,
-          support, support, support,
-          support, support, support,
-        ]
-      ),
-      [0, supportDouble]
-    )
+        let supportDouble = support + (1 << 28)
+        XCTAssertEqual(
+            player.play(
+                [
+                    support, support, support,
+                    support, support, support,
+                    support, support, support,
+                ]
+            ),
+            [0, supportDouble]
+        )
 
-    let supportMax = scout | (0b111 << 28)
-    XCTAssertEqual(
-      player.play(
-        [
-          supportMax, supportMax, supportMax,
-          supportMax, supportMax, supportMax,
-          supportMax, supportMax, supportMax,
-        ]
-      ),
-      [0, support]
-    )
+        let supportMax = scout | (0b111 << 28)
+        XCTAssertEqual(
+            player.play(
+                [
+                    supportMax, supportMax, supportMax,
+                    supportMax, supportMax, supportMax,
+                    supportMax, supportMax, supportMax,
+                ]
+            ),
+            [0, support]
+        )
 
-    let enemy = 0b101010
-    XCTAssertEqual(
-      player.play(
-        [
-          enemy, enemy, enemy,
-          scout, scout, scout,
-          scout, scout, scout,
-        ]
-      ),
-      [1, scout]
-    )
+        let enemy = 0b101010
+        XCTAssertEqual(
+            player.play(
+                [
+                    enemy, enemy, enemy,
+                    scout, scout, scout,
+                    scout, scout, scout,
+                ]
+            ),
+            [1, scout]
+        )
 
-    XCTAssertEqual(
-      player.play(
-        [
-          enemy,      enemy,      enemy,
-          supportMax, supportMax, supportMax,
-          supportMax, supportMax, supportMax,
-        ]
-      ),
-      [1, scout]
-    )
+        XCTAssertEqual(
+            player.play(
+                [
+                    enemy, enemy, enemy,
+                    supportMax, supportMax, supportMax,
+                    supportMax, supportMax, supportMax,
+                ]
+            ),
+            [1, scout]
+        )
 
-    XCTAssertEqual(
-      player.play(
-        [
-          scout, scout, enemy,
-          scout, scout, enemy,
-          scout, scout, enemy,
-        ]
-      ),
-      [5, scout]
-    )
+        XCTAssertEqual(
+            player.play(
+                [
+                    scout, scout, enemy,
+                    scout, scout, enemy,
+                    scout, scout, enemy,
+                ]
+            ),
+            [5, scout]
+        )
 
-    XCTAssertEqual(
-      player.play(
-        [
-          enemy, scout, scout,
-          enemy, scout, scout,
-          enemy, scout, scout,
-        ]
-      ),
-      [3, scout]
-    )
+        XCTAssertEqual(
+            player.play(
+                [
+                    enemy, scout, scout,
+                    enemy, scout, scout,
+                    enemy, scout, scout,
+                ]
+            ),
+            [3, scout]
+        )
 
-    XCTAssertEqual(
-      player.play(
-        [
-          scout, scout, scout,
-          scout, scout, scout,
-          enemy, enemy, enemy,
-        ]
-      ),
-      [7, scout]
-    )
-  }
+        XCTAssertEqual(
+            player.play(
+                [
+                    scout, scout, scout,
+                    scout, scout, scout,
+                    enemy, enemy, enemy,
+                ]
+            ),
+            [7, scout]
+        )
+    }
 }
 
 extension GridWarsPlayerTests {
-  static var allTests : [(String, (GridWarsPlayerTests) -> () throws -> Void)] {
-    return [
-      ("testSimple", testSimple)
-    ]
-  }
+    static var allTests: [(String, (GridWarsPlayerTests) -> () throws -> Void)] {
+        return [
+            ("testSimple", testSimple)
+        ]
+    }
 }
